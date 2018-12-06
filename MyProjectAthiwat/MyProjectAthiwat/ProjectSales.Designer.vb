a7befd9cb01bfadfb299b555188fa2e6e52b69fc
Athiwat <lhumtoey3025@gmail.com>
2018-12-06 13:37:06 +0700

    toey

---
+++ MyProjectAthiwat/MyProjectAthiwat/ProjectSales.Designer.vb
@@ -0,0 +1,239 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ProjectSales
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.btnCal = New System.Windows.Forms.Button()
+        Me.lblComputer = New System.Windows.Forms.Label()
+        Me.lblVist = New System.Windows.Forms.Label()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.lblSales = New System.Windows.Forms.Label()
+        Me.txtComputer = New System.Windows.Forms.TextBox()
+        Me.txtVist = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(421, 401)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabIndex = 7
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'btnCal
+        '
+        Me.btnCal.Location = New System.Drawing.Point(328, 401)
+        Me.btnCal.Name = "btnCal"
+        Me.btnCal.Size = New System.Drawing.Size(75, 23)
+        Me.btnCal.TabIndex = 6
+        Me.btnCal.Text = "Calculate"
+        Me.btnCal.UseVisualStyleBackColor = True
+        '
+        'lblComputer
+        '
+        Me.lblComputer.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.lblComputer.Location = New System.Drawing.Point(452, 260)
+        Me.lblComputer.Name = "lblComputer"
+        Me.lblComputer.Size = New System.Drawing.Size(167, 23)
+        Me.lblComputer.TabIndex = 4
+        Me.lblComputer.Text = " "
+        Me.lblComputer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblVist
+        '
+        Me.lblVist.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.lblVist.Location = New System.Drawing.Point(452, 237)
+        Me.lblVist.Name = "lblVist"
+        Me.lblVist.Size = New System.Drawing.Size(167, 23)
+        Me.lblVist.TabIndex = 3
+        Me.lblVist.Text = " "
+        Me.lblVist.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblTotal
+        '
+        Me.lblTotal.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.lblTotal.Location = New System.Drawing.Point(452, 325)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(167, 23)
+        Me.lblTotal.TabIndex = 5
+        Me.lblTotal.Text = " "
+        Me.lblTotal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblSales
+        '
+        Me.lblSales.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.lblSales.Location = New System.Drawing.Point(452, 163)
+        Me.lblSales.Name = "lblSales"
+        Me.lblSales.Size = New System.Drawing.Size(167, 23)
+        Me.lblSales.TabIndex = 2
+        Me.lblSales.Text = " "
+        Me.lblSales.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'txtComputer
+        '
+        Me.txtComputer.Location = New System.Drawing.Point(455, 125)
+        Me.txtComputer.Name = "txtComputer"
+        Me.txtComputer.Size = New System.Drawing.Size(164, 20)
+        Me.txtComputer.TabIndex = 1
+        '
+        'txtVist
+        '
+        Me.txtVist.Location = New System.Drawing.Point(455, 86)
+        Me.txtVist.Name = "txtVist"
+        Me.txtVist.Size = New System.Drawing.Size(164, 20)
+        Me.txtVist.TabIndex = 0
+        '
+        'Label7
+        '
+        Me.Label7.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Label7.Location = New System.Drawing.Point(216, 325)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(171, 23)
+        Me.Label7.TabIndex = 7
+        Me.Label7.Text = "Commission Total"
+        Me.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Label4.Location = New System.Drawing.Point(182, 201)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(102, 23)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Commission"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label6
+        '
+        Me.Label6.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Label6.Location = New System.Drawing.Point(216, 260)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(171, 23)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "Computer Toshiba Extra520x"
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label8
+        '
+        Me.Label8.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Label8.Location = New System.Drawing.Point(216, 163)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(171, 23)
+        Me.Label8.TabIndex = 10
+        Me.Label8.Text = "Sales"
+        Me.Label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Label3.Location = New System.Drawing.Point(216, 125)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(171, 23)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Computer Toshiba Extra520x"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label5
+        '
+        Me.Label5.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Label5.Location = New System.Drawing.Point(216, 237)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(171, 23)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "Vist Gluta"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Label2.Location = New System.Drawing.Point(216, 86)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(171, 23)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "Vist Gluta"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label1.Location = New System.Drawing.Point(280, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(245, 28)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "โปรแกรมคำนวณยอดขายสินค้า"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'ProjectSales
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnCal)
+        Me.Controls.Add(Me.lblComputer)
+        Me.Controls.Add(Me.lblVist)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.lblSales)
+        Me.Controls.Add(Me.txtComputer)
+        Me.Controls.Add(Me.txtVist)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "ProjectSales"
+        Me.Text = "ProjectSales"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnClose As Button
+    Friend WithEvents btnCal As Button
+    Friend WithEvents lblComputer As Label
+    Friend WithEvents lblVist As Label
+    Friend WithEvents lblTotal As Label
+    Friend WithEvents lblSales As Label
+    Friend WithEvents txtComputer As TextBox
+    Friend WithEvents txtVist As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+End Class
---
+++ MyProjectAthiwat/MyProjectAthiwat/ProjectSales.vb
@@ -0,0 +1,23 @@
+﻿Public Class ProjectSales
+    Private Sub btnCal_Click(sender As Object, e As EventArgs) Handles btnCal.Click
+        Dim Vist, computer, sales, commission, commissVist, commissCom As Double
+
+        Vist = Val(txtVist.Text)
+        computer = Val(txtComputer.Text)
+
+        commissVist = Vist * 5 / 100
+        commissCom = computer * 10 / 100
+        commission = commissVist + commissCom
+
+        sales = Vist + computer
+        lblSales.Text = sales
+        lblVist.Text = commissVist
+        lblComputer.Text = commissCom
+        lblTotal.Text = commission
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Me.Close()
+
+    End Sub
+End Class
